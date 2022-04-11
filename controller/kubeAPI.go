a7b490f878f4ae300f86224a761208cbd146285a
idobry <idob@ip-172-20-140-6.eu-west-1.compute.internal>
2022-04-11 09:40:42 +0300

    init

--- controller/kubeAPI.go
+++ controller/kubeAPI.go
@@ -9,6 +9,7 @@ import (
 	"github.com/google/uuid"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	//ingcorev1 "k8s.io/client-go/kubernetes"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	networkingv1 "k8s.io/api/networking/v1"
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	k8s.io/api v0.23.5
 	k8s.io/apimachinery v0.23.5
 	k8s.io/client-go v0.23.5
+
 )
 
 require (
