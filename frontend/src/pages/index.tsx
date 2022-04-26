5783da8d1a3769913c95c9e555f04eedc27140af
maorgoldberg <maorgoldberg3@gmail.com>
2022-04-26 11:16:38 +0300

    added back slash

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -51,7 +51,7 @@ const Index = () => {
               </button>
             </div>
             {load ? <Loading></Loading>: null}
-            {tty ? <iframe className='w-[90%] h-[90%] m-auto' src={WETTY_URL + tty} title="W3Schools Free Online Web Tutorials"></iframe> : null}
+            {tty ? <iframe className='w-[90%] h-[90%] m-auto' src={WETTY_URL + tty + "/"} title="W3Schools Free Online Web Tutorials"></iframe> : null}
             {tty ? <p>{tty}</p> : null}
           </div>
         </div>
